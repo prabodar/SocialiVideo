fc428a48ffb887379fd727cb3166a50e96663dc8
praboda <prabodar@gmail.com>
2016-01-22 21:47:03 +0100

    change URl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def youtube_video(url)
-    render :partial => 'shared/youtube', :locals => { :url => url }
-  end
 
   private
   def current_user
