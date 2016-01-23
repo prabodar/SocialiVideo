24f4a7d43e838c9fcf3832a0cd374781a7617ba7
praboda <prabodar@gmail.com>
2016-01-23 17:30:25 +0100

    change URl

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -248,8 +248,8 @@ class VideosController < ApplicationController
 
       #system ("viddl-rb #{url} --save-dir ~/shrouded-reef-66672/resources/public/Video")
 
-      system ("viddl-rb #{url} --save-dir ~/Video")
-      system ( "rename s/ /_/g ~/Video/*")
+      system ("viddl-rb #{url} --save-dir ~/public/Video/")
+      system ( "rename s/ /_/g ~/public/Video/*")
 
       #remove name spaces with underscore
       #system ("mv ~/ecousin-tsp-fb/public/Video/'#{file}' ~/ecousin-tsp-fb/public/Video/'#{video_name}'")
